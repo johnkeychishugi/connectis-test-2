6da737336e7dd04a27c14778eed4a20e86ece2f2
John Chishugi <jkchishugi@gmail.com>
2020-07-14 17:16:20 +0200

    fx-product-router

--- routers/produits.js
+++ routers/produits.js
@@ -2,7 +2,6 @@ import express from 'express';
 import produitController from '../controllers/produitController';
 import authCheck from '../middlewares/authCheck';
 
-
 const routers = express.Router();
 
 routers.get("/produits",authCheck,produitController.getProduits);
